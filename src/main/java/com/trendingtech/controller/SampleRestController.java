97b1017c10f810c07587f6999865419a211e9b02
Kakarla V S Ravindra Reddy <kvsravindrareddy@gmail.com>
2019-10-19 21:05:27 +0530

    Update SampleRestController.java

--- src/main/java/com/trendingtech/controller/SampleRestController.java
+++ src/main/java/com/trendingtech/controller/SampleRestController.java
@@ -2,6 +2,7 @@ package com.trendingtech.controller;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,19 +11,13 @@ import com.trendingtech.inf.RetrofitInterface;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Response;
-import retrofit2.Retrofit;
 
 
 @RestController
 public class SampleRestController {
 
+	@Autowired
 	private RetrofitInterface retrofitInterface;
-	
-	public SampleRestController()
-	{
-		Retrofit retrofit = new Retrofit.Builder().baseUrl("http://localhost:7001").build();
-		retrofitInterface = retrofit.create(RetrofitInterface.class);
-	}
 	
 	@GetMapping(value="/retrofittest")
 	public String hello() throws IOException
